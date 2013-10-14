58fa720fb6b129d61f305d5f7cc53ae6689b0cee
Tom Lee <ApigeeCorporation@TomMacBookAir.local>
2013-10-14 16:21:52 -0700

    add files

---
+++ AndroidPushNotifications/PushNotificationTest/src/com/apigee/test/pushandroid/TestPushNotificationAndroid.java
@@ -0,0 +1,60 @@
+package com.apigee.test.pushandroid;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.apigee.test.base.TestLogger;
+import com.apigee.test.base.BasedBrowser;
+import com.apigee.test.lib.Library;
+import com.apigee.test.util.Util;
+import com.thoughtworks.selenium.Selenium;
+
+/**
+ * The driver class to start the Android push notification test          
+ */
+
+public class TestPushNotificationAndroid extends BasedBrowser{
+	
+	static int failedCount=0;
+	Util util = new Util();
+	Library library=new Library();
+	protected Log log = TestLogger.getLogger(this.getClass());
+	
+	@Parameters({"baseurl", "screenshotdir","testdataDir"})
+	@Test(enabled=true)
+	public void testPushNotificationAndroid(String baseurl, String screenshotdir, String testdataDir) throws IOException {
+		
+		Selenium browser = getBrowser();
+
+		try{
+		
+			log.info("Start to test Android push notifications");
+			
+			if (library.login(util.getXpath("loginPath","Account"),util.getXpath("aemailUpdate","Account"),util.getXpath("apasswordUpdate","Account"), browser, log)){
+				library.appPushNotification(browser,"Android",log);
+				library.goBacktoDashboard(browser,log);
+				library.logout(browser, log);
+				stop();
+				System.exit(0);
+			}else{
+				Exception e = new Exception("Failed to log in the account");
+                throw e;
+			}
+			
+			//Dit environment
+			/*if (library.loginMobileAnalytics("/accounts/sign_in", "","", browser, log)){
+				library.logoutMobileAnalytics(browser, log);
+			}*/
+			
+			log.info("End to test Android push notifications");
+			
+		}catch(Exception e){
+			failedCount++;
+			util.takeScreenShot ("testPushNotificationAndroid ", browser, failedCount, screenshotdir);
+			log.error("Error in testPushNotificationAndroid "+util.getStackTrace(e));
+		}
+	}
+}
---
+++ AndroidPushNotifications/PushNotificationTest/src/com/apigee/test/pushios/TestPushNotificationiOS.java
@@ -0,0 +1,55 @@
+package com.apigee.test.pushios;
+
+import java.io.IOException;
+import org.apache.commons.logging.Log;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import com.apigee.test.base.BasedBrowser;
+import com.apigee.test.base.TestLogger;
+import com.apigee.test.lib.Library;
+import com.apigee.test.util.Util;
+import com.thoughtworks.selenium.Selenium;
+
+public class TestPushNotificationiOS  extends BasedBrowser{
+	
+	static int failedCount=0;
+	Util util = new Util();
+	Library library=new Library();
+	protected Log log = TestLogger.getLogger(this.getClass());
+	
+	@Parameters({"baseurl", "screenshotdir","testdataDir"})
+	@Test(enabled=true)
+	public void testPushNotificationiOS(String baseurl, String screenshotdir, String testdataDir) throws IOException {
+		
+		Selenium browser = getBrowser();
+
+		try{
+		
+			log.info("Start to test iOS push notifications");
+			
+			if (library.login(util.getXpath("loginPath","Account"),util.getXpath("emailUpdate","Account"),util.getXpath("passwordUpdate","Account"), browser, log)){
+				library.appPushNotification(browser,"iOS",log);
+				//library.goBacktoDashboard(browser,log);
+				//library.logout(browser, log);
+				stop();
+				System.exit(0);
+			}else{
+				Exception e = new Exception("Failed to log in the account");
+                throw e;
+			}
+			
+			//Dit environment
+			/*if (library.loginMobileAnalytics("/accounts/sign_in", "","", browser, log)){
+				library.logoutMobileAnalytics(browser, log);
+			}*/
+			
+			log.info("End to test iOS push notifications");
+			
+		}catch(Exception e){
+			failedCount++;
+			util.takeScreenShot ("testPushNotificationiOS ", browser, failedCount, screenshotdir);
+			log.error("Error in testPushNotificationiOS "+util.getStackTrace(e));
+		}
+	}
+}
+
