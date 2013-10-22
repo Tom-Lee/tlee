4dec755b60fa5c1a963f5a592b29a580d36aad8f
Tom Lee <ApigeeCorporation@TomMacBookAir.local>
2013-10-22 09:47:39 -0700

    add

---
+++ MobileAnalyticsTest/src/com/apigee/test/base/TestLogger.java
@@ -0,0 +1,43 @@
+package com.apigee.test.base;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+/*
+ * Purpose: define the logging methods
+ * Author: Tom Lee
+ * @Apigee Corporation
+ */
+public class TestLogger {
+	 
+	  static {
+	    try {
+	        org.apache.log4j.BasicConfigurator.configure();
+	    }
+	    catch (Exception ex) {
+	      System.err.println("Exception initializing log4j " + ex.getMessage());
+	      ex.printStackTrace(System.err);
+	      throw new ExceptionInInitializerError(ex);
+	    }   
+	  }
+	  
+	  /**
+	   * @param clazz
+	   * @return org.apache.log4j.Logger
+	   */
+	  public static Log getLogger(Class clazz) {
+	    Log logger = (Log) LogFactory.getFactory().getInstance(clazz);
+
+	    return logger;   
+	  }
+	  
+	  /**
+	   * @param loggerName
+	   * @return org.apache.commons.logging.Log
+	    */
+	  public static Log getLogger(String loggerName) {
+	    Log logger = (Log) LogFactory.getFactory().getInstance(loggerName);
+
+	    return logger;   
+	  }
+
+}
